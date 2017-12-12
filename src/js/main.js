c5e24b67d0f1c043b5c34d535bc7a4c74cd1d870
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-12-12 16:04:36 +0700

    clients

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+
+// clients slider
+const clientsSlider = new Swiper('.clients-slider__container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
