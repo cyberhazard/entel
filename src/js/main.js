e9d5b949b5dc8105e82dc14900a2e246e6e2ac7e
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-12-15 01:10:18 +0700

    slider header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
+//header slider
+const headerSlider = new Swiper('.header-slider__container', {
+  spaceBetween: 0,
+  slidesPerView: 1.3,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: '.header-slider__next',
+    prevEl: '.header-slider__prev',
+  },
+  pagination: {
+    el: '.header-slider__pagination',
+  },
+  breakpoints: {
+    // when window width is <= 320px
+    660: {
+      slidesPerView: 1
+    },
+  }
+})
+
 
 // clients slider
 const clientsSlider = new Swiper('.clients-slider__container', {
@@ -8,4 +29,3 @@ const clientsSlider = new Swiper('.clients-slider__container', {
     prevEl: '.swiper-button-prev',
   },
 });
-
